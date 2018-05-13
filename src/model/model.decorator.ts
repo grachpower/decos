ba3646cefc115c8030f75b44a64371bf54181bc8
Artyom Andreev <amiscary@gmail.com>
2018-05-13 21:40:08 +0300

    update model decorator

--- src/model/model.decorator.ts
+++ src/model/model.decorator.ts
@@ -1,14 +1,16 @@
 import { getAutowired } from '../autowired/autowired.decorator';
 import { getMappedClass } from '../mapped-class/mapped-class.decorator';
 
-function handler() {
+function handler(allowStrictMode: boolean) {
     return {
         get(target, prop) {
-            if (prop in target) {
-                console.log(`${prop} найдено`);
-            } else {
-                console.log(`${prop} не найдено`);
-                throw new Error(`Property '${prop}' is not a part of target model`);
+            if (allowStrictMode) {
+                if (prop in target) {
+                    console.log(`${prop} найдено`);
+                } else {
+                    console.log(`${prop} не найдено`);
+                    throw new Error(`Property '${prop}' is not a part of target model`);
+                }
             }
 
             console.log(`Чтение ${prop}`);
@@ -16,11 +18,13 @@ function handler() {
             return target[prop];
         },
         set(target, prop, value) {
-            if (prop in target) {
-                console.log(`${prop} найдено`);
-            } else {
-                console.log(`${prop} не найдено`);
-                throw new Error(`Property '${prop}' is not a part of target model`);
+            if (allowStrictMode) {
+                if (prop in target) {
+                    console.log(`${prop} найдено`);
+                } else {
+                    console.log(`${prop} не найдено`);
+                    throw new Error(`Property '${prop}' is not a part of target model`);
+                }
             }
 
             console.log(`Запись ${prop} ${value}`);
@@ -32,18 +36,29 @@ function handler() {
     };
 }
 
-export function Model(): Function {
+/**
+ * allowStrictMode: boolean; true by default
+ */
+export interface ModelConstructorInterface {
+    allowStrictMode: boolean;
+}
+
+export function Model({allowStrictMode = true}: ModelConstructorInterface): Function {
     /* tslint:disable:only-arrow-functions*/
     return function<T extends {new(...args: any[]): {}}>(targetConstructor: T): Function  {
         return class extends targetConstructor {
             constructor(...params) {
                 super(params);
 
-                return new Proxy(this, handler());
+                return new Proxy(this, handler(allowStrictMode));
             };
 
             public resolveParams(params?: any): void {
                 params.forEach((value: any, key: string) => {
+                    if (!(key in this) && allowStrictMode) {
+                        throw new Error(`Property '${prop}' is not a part of target model`);
+                    }
+
                     if (!Array.isArray(value)) {
                         const autowiredClass: new (data: any) => any = getAutowired(this, key);
 
