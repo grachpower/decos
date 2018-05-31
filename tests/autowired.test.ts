7b6e7fb9a1879a5070d91afa25e192c3195867af
Artyom Andreev <amiscary@gmail.com>
2018-05-31 22:32:39 +0300

    add tests

--- tests/autowired.test.ts
+++ tests/autowired.test.ts
@@ -23,6 +23,18 @@ class TestingModel {
     }
 }
 
+@Model()
+class CycleDepModel {
+    @prop id: number;
+    @prop name: string;
+
+    @Autowired(CycleDepModel) cycleChild: CycleDepModel;
+
+    constructor(params?) {
+        (this as any).resolveParams(params);
+    }
+}
+
 test('Should add @Autowired child models', () => {
     const testingParams = {
         id: 24,
@@ -31,11 +43,30 @@ test('Should add @Autowired child models', () => {
             {data: 'foo'},
             {data: 'bar'},
         ],
-        child: {data: 'foo'},
+        child: {
+            id: 26,
+            name: 'User',
+        },
     };
 
     const model = new TestingModel(testingParams);
 
     expect(model.child instanceof Child).toBeTruthy();
     expect('children' in model).toBeFalsy();
+});
+
+test('Should work with cycle dependencies', () => {
+    const testingParams = {
+        id: 24,
+        name: 'D11ke',
+        children: [
+            {data: 'foo'},
+            {data: 'bar'},
+        ],
+        cycleChild: {data: 'foo'},
+    };
+
+    const model = new CycleDepModel(testingParams);
+
+    expect(model.cycleChild instanceof CycleDepModel).toBeTruthy();
 });
